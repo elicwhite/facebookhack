d5e1f1dfe8bd82d8b3b3f0a1649467ac2239637a
TheSavior <github@eli-white.com>
2013-04-20 11:30:56 -0700

    Moving mutual friends to the right

--- www/templates/page.tpl.php
+++ www/templates/page.tpl.php
@@ -49,36 +49,35 @@
           </div>
 
         </div>
-        <!-- Mutual Friend list -->
-        <!-- <div class="row"> -->
-        <h3>Mutual Friends</h3>
-        <?php
-        foreach ($PAGE_VARS['types']['mutualFriends'] as $friend) {
-        ?>
-        	<div class="mutualfriend">
-            <img src="<?=$friend->picture->data->url?>"/>
-        	</div>
-        	<?php	
-        }
-        ?>
-        <!-- </div> -->
       </div>
       <!-- Status feeds -->
       <div class="span4">
-        <h3>Popular Statuses</h3>
-      	<?php
-      	foreach ($PAGE_VARS['types']['status'] as $status) {
-      		?>
-  		  <div class="status">
-          <p><?=$status["original"]->message ?></p>
-          <div class="statusnums">      
-            <img src="img/facebook-thumbs-up.png" >     
-            <?= $status['likes'] ?>     
+      	<div class="statuses">
+          <?php
+        	foreach ($PAGE_VARS['types']['status'] as $status) {
+        		?>
+    		  <div class="status">
+            <p><?=$status["original"]->message ?></p>
+            <div class="statusnums">      
+              <img src="img/facebook-thumbs-up.png" >     
+              <?= $status['likes'] ?>     
+            </div>
           </div>
+        		<?php
+        	}
+          ?>
         </div>
-      		<?php
-      	}
-      	?>
+        <div class="mutualFriends">
+          <?php
+          foreach ($PAGE_VARS['types']['mutualFriends'] as $friend) {
+          ?>
+            <div class="mutualfriend">
+              <img src="<?=$friend->picture->data->url?>"/>
+            </div>
+            <?php 
+          }
+          ?>
+        </div>
       </div>  
     </div>
     <div class="row">
