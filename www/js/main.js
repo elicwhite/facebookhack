97c556cf8f179a9470cb3703b2c5405466238261
ashishsc <ashish.s.chandwani@gmail.com>
2013-04-20 10:08:29 -0700

    hacked status spacing

--- www/js/main.js
+++ www/js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var IS_LOGGED_IN = false; 
+var IS_LOGGED_IN = false;
+var STATUS_SPACING = '80px';
 $(document).ready(function(){
   if (IS_LOGGED_IN) {
     $("#intro").hide();
@@ -9,7 +10,7 @@ $(document).ready(function(){
   	$('#login-username').hide();
     
   }
-
+  $('div.status:not(div.status:first-child)').css('margin-top', STATUS_SPACING);
   // $(".large").mouseover(function(){
   // 	$(".caption").show();
   // });
