71772051be0a523eeceaf1b932adb94e79f614bc
Evan Cohen <evanbtcohen@gmail.com>
2013-04-19 23:00:02 -0700

    swag

--- www/templates/head.php
+++ www/templates/head.php
@@ -4,18 +4,15 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>Saros Framework</title>
 	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico" />
-	<?php echo $this->headStyles()->appendFile("css/main.css") ?>
-	<?php echo $this->headScripts() ?>
+	<link type="text/css" rel="stylesheet" media="all" href="css/bootstrap.min.css" />
+	<link type="text/css" rel="stylesheet" media="all" href="css/bootstrap-responsive.min" />
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 </head>
 <body>
 	<div id="container">
 		<div id="main">
-			<?php
-            echo $this->content() 
-            ?>
 		</div>
 		<div id="footer">
-            <?php echo \Spot\Log::queryCount() ?> Queries
 		</div>
 	</div>
 </body>
