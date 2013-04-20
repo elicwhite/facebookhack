dcb428f129f3aa86178a081df3dd93abcaeb307d
Evan Cohen <evanbtcohen@gmail.com>
2013-04-20 07:41:26 -0700

    stauts updates

--- www/templates/page.tpl.php
+++ www/templates/page.tpl.php
@@ -24,7 +24,7 @@
         foreach ($PAGE_VARS['types']['mutualFriends'] as $friend) {
         	?>
         	<div class="mutualfriend">
-	            <img src="<?=$friend->picture->data->url?>"/>;
+	            <img src="<?=$friend->picture->data->url?>"/>
           	</div>
         	<?php	
         }
@@ -33,22 +33,16 @@
       </div>
       <!-- Status feeds -->
       <div class="span4">
-        <div class="status">
-          <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+      	<?php
+      	foreach ($PAGE_VARS['types']['status'] as $status) {
+      		?>
+  		<div class="status">
+          <p><?=$status["original"]->message ?></p>
           <div class="statusnums">No likes, loser!</div>
         </div>
-        <div class="status">
-          <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
-          <div class="statusnums">No likes, loser!</div>
-        </div>
-        <div class="status">
-          <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
-          <div class="statusnums">No likes, loser!</div>
-        </div>
-        <div class="status">
-          <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
-          <div class="statusnums">No likes, loser!</div>
-        </div>
+      		<?php
+      	}
+      	?>
       </div>  
     </div>
     <div class="row">
